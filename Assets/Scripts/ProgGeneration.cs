95d356ba31ecd5f72b2f0028878455114dfb7afb
Guizuka <51218840+Guizuka@users.noreply.github.com>
2022-02-09 10:16:23 -0500

    Fixing Programatic Generation

--- Assets/Scripts/ProgGeneration.cs
+++ Assets/Scripts/ProgGeneration.cs
@@ -3,32 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-
-public class Customer
-{
-    public float sTime;
-    public float aTime;
-}
-
 public class ProgGeneration : MonoBehaviour
 {
 
-    float mean = 1f;
-    float stdDev = 0.1f;
-    public Customer[] customers;
+    float mean = 4f;
+    float stdDev = 1f;
+   
     // Start is called before the first frame update
     void Start()
     {
-        customers = new Customer[50];
-        //customers[0].sTime = GenerateNormalized(mean, stdDev);
-        //customers[0].aTime = GenerateNormalized(mean, stdDev);
-        Debug.Log("Customer" + "0" + "Spent time:" + customers[0].sTime + "Arrival time" + customers[0].aTime);
-        //for (int i = 0; i < 50; i++)
-        //{
-        //    customers[0].sTime = GenerateNormalized(mean, stdDev);
-        //    customers[0].aTime = GenerateNormalized(mean, stdDev);
-        //    Debug.Log("Customer" + "0" + "Spent time:" + customers[0].sTime + "Arrival time" + customers[0].aTime);
-       // }
+        GenerateCustomers();
     }
 
     // Update is called once per frame
@@ -44,15 +28,25 @@ public class ProgGeneration : MonoBehaviour
         
     }
 
+    private void GenerateCustomers()
+    {
+        Customer[] customers = new Customer[50];
+        for (int i = 0; i < 50; i++)
+        {
+            customers[i] = new Customer();
+            customers[i].sTime = GenerateNormalized(mean, stdDev);
+            customers[i].aTime = GenerateNormalized(mean, stdDev);
+            Debug.Log("Customer " + i + " Spent time: " + customers[i].sTime + " Arrival time: " + customers[i].aTime);
+        }
+    }
+
 
 
     private float GenerateNormalized(float mean, float stddev)
     {
         float var = stddev * stddev;
         System.Random rnd = new System.Random();
-
         double x = rnd.NextDouble();
-
         float one_over_2pi = (float)(1.0 /(stddev * Math.Sqrt(2 * Math.PI)));
         return (float)(one_over_2pi * Math.Exp(-(x - mean) * (x - mean) / (2 * var)));
     }
